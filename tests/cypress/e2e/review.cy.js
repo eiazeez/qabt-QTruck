842308a3ad5d966950151e10167da6ace51f5a6c
byazeez <isaac.douglas08@gmail.com>
2022-09-05 18:31:15 -0300

    Validando os comentários inseridos

--- tests/cypress/e2e/review.cy.js
+++ tests/cypress/e2e/review.cy.js
@@ -8,11 +8,9 @@ describe('Avaliações', ()=> {
     it ('Deve enviar uma nova avaliação', ()=> {
 
         const user = {
-
             name: 'Madru Gada',
             instagram: 'madruguinha',
             password: 'pwd123'
-
         }
 
         const foodtruck = {
@@ -36,8 +34,66 @@ describe('Avaliações', ()=> {
         cy.uiLogin(user)
 
         MapPage.goToFoodtruck(foodtruck.name)
-        FoodTruckPage.addReview(review)
 
+        FoodTruckPage.addReview(review)
+        FoodTruckPage.reviewConfirm(user, review)
+        
+    })
+
+    it ('Deve validar todos os comentários caso haja mais de 1 comentário', ()=> {
+        const user = {
+            name: 'Steve Spilberg',
+            instagram: 'stevinho',
+            password: 'pwd123'
+        }
+
+        const user2 = {
+            name: 'Tim Berling ',
+            instagram: 'Avicii',
+            password: 'pwd123'
+        }
+
+        const foodtruck = {
+            latitude: '-3.7035751789173963',
+            longitude: '-38.56732232300903',
+            name: 'Tomorrowland',
+            details: 'A terra do amanhã',
+            opening_hours: 'Das 08h às 22h',
+            open_on_weekends: false
+        }
+
+        const review = {
+            comment: 'O taco estava divino, porém, caro.',
+            stars: 3
+        } 
+
+        const review2 = {
+            comment: 'Achei meio meme',
+            stars: 1
+        } 
+
+        cy.apiCreateUser(user)
+        cy.apiLogin(user)
+        cy.apiCreateFoodTruck(foodtruck)
+
+        cy.apiCreateUser(user2)
+        cy.apiLogin(user2)
+
+        cy.uiLogin(user)
+
+        MapPage.goToFoodtruck(foodtruck.name)
+
+        FoodTruckPage.addReview(review)
+        FoodTruckPage.reviewConfirm(user, review)
+
+        cy.clearLocalStorage()
+        cy.uiLogin(user2)
+
+        MapPage.goToFoodtruck(foodtruck.name)
+
+        FoodTruckPage.addReview(review2)
+        FoodTruckPage.reviewConfirm(user2, review2)
+        
     })
 
 })
--- tests/cypress/support/pages/Foodtruck/index.js
+++ tests/cypress/support/pages/Foodtruck/index.js
@@ -9,6 +9,30 @@ class FoodTruckPage {
         cy.contains('button', 'Enviar avaliação').click()
 
     }
+
+    reviewConfirm (user, review) {
+
+    cy.contains('.profile .details strong', user.name)
+        .should('be.visible')
+        .should('have.text', user.name)
+
+    cy.contains('.profile .details span', user.instagram)
+        .should('be.visible')
+        .should('have.text', user.instagram)
+
+    cy.contains('.comment p', review.comment)
+        .should('be.visible')
+        .should('have.text', review.comment)
+
+    cy.contains('.profile .details span', user.instagram)
+        .parent()
+        .parent()
+        .siblings('.stars')
+        .find('svg')
+        .should('be.visible')    
+        .should('have.length', review.stars)
+
+    }
     
 }
 
